529e6ed5283d6436dbe415d45ee3fcac181b8f36
krishan <krishanb281988@gmail.com>
2022-10-18 22:32:44 +0530

    upload shebang.sh file

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -3,3 +3,10 @@
 echo hello world
 echo line 1
 echo line 2
+
+# Special Characters in linux for specific purpose.
+# /n  : cursor moves to next 
+# /t  : cursor moves to tab space 
+# Whenever you want to use special characters, ensure you enbale them with -e option in echo
+
+echo line1 /n line2
