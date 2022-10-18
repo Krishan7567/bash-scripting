befda00eaa66212a323b9c95c70efcfd03ebcf37
krishan <krishanb281988@gmail.com>
2022-10-18 23:32:37 +0530

    upload shebang.sh file

--- 03-variable.sh
+++ 03-variable.sh
@@ -2,3 +2,25 @@
 
 # Variable is something which holds the values dynamically.
 
+a=10 
+b=20 
+# c=1000   # local variables
+
+# No concept of data types in shell-scripting.
+# Everything is considered sa string by default.
+
+echo $a 
+echo ${a} 
+echo "$a"
+echo "${b}"
+echo '${a}'
+
+
+
+# Whenever you use single quotes on variable, the power of special characters will be taken down.
+# Here $ is a special character to print variables.
+
+# rm -rf /data/${OPT_VARIABLE}  # rm -rf /data/
+
+
+# Note: local variables will have higher priority than export variables ( environment variables )
