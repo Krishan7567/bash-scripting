24264d9e48630fb73525b7838058f890402092d9
krishan <krishanb281988@gmail.com>
2022-10-19 23:20:02 +0530

    function file uploaded

--- 07-function.sh
+++ 07-function.sh
@@ -16,6 +16,7 @@ sample() {
 stat() {
     echo "Load average on the system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
     echo "Number of user signed in is : $(who |wc -l)"
+    echo "Stat is completed"
 }
 
 stat
