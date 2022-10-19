1fefb9dd58f3fe043fbfdde13c0dc0a0dfe5adc8
krishan <krishanb281988@gmail.com>
2022-10-20 01:04:39 +0530

    function file uploaded

--- 10-if.sh
+++ 10-if.sh
@@ -21,3 +21,21 @@ fi
 if [ -z $b ] ; then 
     echo "variable b is empty"
 fi  
+
+
+# Demo on else if 
+c=$1 
+
+if [ "$c" -eq "10" ] ; then 
+    echo "value of c is 10"
+
+elif [ "$c" -eq "20" ] ; then 
+    echo "value of c is 20"
+
+elif [ "$c" -eq "30" ] ; then 
+    echo "value of c is 30"
+
+else 
+    echo "Value is not 10 or 20 or 30"
+
+fi 
