0131cbf0f04bc5efe05f66a0026988646745faf4
krishan <krishanb281988@gmail.com>
2022-10-20 00:10:47 +0530

    function file uploaded

---
+++ 09-case.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# case $var in
+
+#   opt1) commands ;;
+#   opt2) commands ;;
+
+# esac
+
+ACTION=$1
+
+case $ACTION in
+
+    start)
+        echo "Starting abc services"
+        ;;
+    stop)
+        echo "Stoping abc services"
+        ;;
+    *)
+        echo "Valid options are either start or stop"
