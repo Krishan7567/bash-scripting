aa7ed472c730561ff7c4257dfc754baf2b9cc530
krishan <krishanb281988@gmail.com>
2022-10-20 00:49:57 +0530

    function file uploaded

--- 10-if.sh
+++ 10-if.sh
@@ -7,5 +7,5 @@ a="abcd"
 if  [ "$a" == "abc" ]; then 
     echo -e "\e[32m Both of the are equal \e[0m"
 else 
-     echo -e "\e[31m Both of the are not equal \e[0m"
+     echo -e "\e[31m Both of them are not equal \e[0m"
 fi 
