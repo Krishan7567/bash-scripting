7cee9b5eed357bf19e33c23834ad1df5964ffc59
krishan <krishanb281988@gmail.com>
2022-10-22 00:47:13 +0530

    file uploaded

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -20,6 +20,10 @@ fi
 echo "Downloading the component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+fi
+
 echo "Performing cleanup:"
 cd /usr/share/nginx/html
 
@@ -29,6 +33,10 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+fi
 
 systemctl enable nginx
 systemctl start nginx
