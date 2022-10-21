237cedc37bcc6b2b3210ea9ced5eb48f474a2eb3
krishan <krishanb281988@gmail.com>
2022-10-22 00:27:37 +0530

    file uploaded

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo "I am frontend component"
+set -e
+
+# verify the script is been executed as root user
+userid=$(id -u)
+
+if [$userid -ne 0] ; then
+    echo -e "\e[31m you must run the script as a root user or sudo previledge \e[0m"
+    exit 1
+fi
+
+
+yum install nginx -y
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+
+cd /usr/share/nginx/html
+
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+systemctl start nginx
