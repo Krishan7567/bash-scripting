f1ae9df882bd2bc069c223a197a99c66be394b41
krishan <krishanb281988@gmail.com>
2022-10-22 01:03:38 +0530

    file uploaded

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -24,28 +24,24 @@ stat
 
 echo -n "Downloading the component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
+stat
 
 echo -n "Performing cleanup:"
 cd /usr/share/nginx/html
-
 rm -rf *
+stat
+
+echo -n "Unzipping the components"
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
+stat
+
+echo -n "Configuring the reverse proxy"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat
 
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
 
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/frontend.log
